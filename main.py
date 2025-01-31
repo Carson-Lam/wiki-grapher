eb3890e0db20974c67ff781dec431414baec927f
Carson Lam <lamcn51@gmail.com>
2025-01-31 02:42:25 -0500

    Added local file scraping functionality

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from bs4 import BeautifulSoup
 
-with open('home.html', 'a') as html_file:
-    content = html_file.read()
-    print(content)
+with open('home.html', 'r') as html_file:
+    content = html_file.read()    
+    soup = BeautifulSoup(content, 'lxml')
+    section_cards = soup.find_all('div', class_='card')
+    for section in section_cards:
+        section_name = section.h5.text
+        section_price = section.a.text.split()[-1] #spl its text by spaces and puts into array.
+                                                 #-1 index grabs the last array element
+        
+        print(f'{section_name} has {section_price}')
+
