d022a8ef1b81cfbcbdd0f6b8266dba5ec333142c
Carson Lam <lamcn51@gmail.com>
2025-02-01 23:56:58 -0500

    Added country and region extraction

--- webScraper.py
+++ webScraper.py
@@ -5,9 +5,14 @@ import requests
 url = 'https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
+table = soup.find('table', class_= 'wikitable')
+rows = table.find_all('tr')[1:]
+for row in rows: 
+    cols=row.find_all('td')
+    if cols: #ensures row has data (skip empty rows)
+        country=cols[0].text.strip()
+        region = cols[5].text.strip()
+        print("Country: " + country + "  Region: " + region)
 
-countries = soup.find_all('tr')
-for country in countries: 
-    print(country)
 
 
