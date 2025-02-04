e7c99bd86acdb573b8ba41d3a356947bd8e903cd
Carson Lam <lamcn51@gmail.com>
2025-02-04 01:36:59 -0500

    Fixed link detection for bullet points

--- webScraper.py
+++ webScraper.py
@@ -1,26 +1,36 @@
+# Author: Carson Lam
+
 from bs4 import BeautifulSoup
 import requests
 import re
 
-# Wikipedia page "Fungus" used for testing due to length
-url = 'https://en.wikipedia.org/wiki/Fungus'
+# Testable inputs:
+# Longer: https://en.wikipedia.org/wiki/Fungus
+# Shorter: https://en.wikipedia.org/wiki/Rozellida
+# Shortest: https://en.wikipedia.org/wiki/Marie_Maxime_Cornu
+
+# Build the soup object
+url = 'https://en.wikipedia.org/wiki/Marie_Maxime_Cornu'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 
 # Body and stop point handling
-stop_element = soup.find('h2', id='See_also')  # Find stop point
-title = soup.find('h1', id = 'firstHeading')
-body = soup.find_all(True) # Get all body elements
+stop_element = soup.select_one('h2#See_also, h2#References') # stop at either See Also/References
+title = soup.find('h1', id = 'firstHeading')                 # (some pages have no See Also)
+body = [title] + list(title.find_all_next(True)) # Get all body elements
 
 # Link storage
 seen_links = set()
 unique_links = []
 
+# Helper method for appending unique links
 def add_unique_links(elements):
     for element in elements:
         href = element.get('href')
         img = element.find('img')
-        if href and not img and href not in seen_links: # Ensure link is unique, exists, and has no image
+
+        # Ensure link exists, has no image, is not internal, and unique
+        if href and not img and not re.match(r'^/wiki/(Wikipedia:|Talk:|Special:)', href) and href not in seen_links: 
             seen_links.add(href)                      
             unique_links.append(element)
 
@@ -32,14 +42,17 @@ for element in body:
         print(f'Extracted title {title.text}')
 
     # Add links retrieved from paragraphs and image captions
-    if element.name == "p" or element.name == "figcaption":
-        add_unique_links(element.find_all('a', href=re.compile('/wiki/')))
+    if element.name == 'p' or element.name == 'figcaption' or element.name == 'ul':
+        add_unique_links(element.find_all('a', href=re.compile('^/wiki/')))
 
     # Add links retrieved from table
-    if element.name == "table":
+    if element.name == 'table':
         for row in element.find_all('tr'): 
-            add_unique_links(row.find_all('a', href=re.compile('/wiki/')))
+            add_unique_links(row.find_all('a', href=re.compile('^/wiki/')))
 
 for link in unique_links:
     print(f'Extracted link {link.get('href')} with title {link.get('title')}')
+
+
+
  
